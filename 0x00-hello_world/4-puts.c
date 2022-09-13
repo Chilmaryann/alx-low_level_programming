5254125b115d30051473cc7163c1dd7868dab347
maryann <chilmaryann@yahoo.com>
2022-09-12 21:48:33 -0700

    initial commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #!/bin/bash
 #include <stdio.h>
 /**
- * description: main - prints out a string to stdout.
+ * Description: main - Prints out a string to stdout.
  * Return: 0 if success.
  */
 int main(void)
-{
-puts("\"programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
